691ed888b1a0573da00eb0bd0b6c7f51cc57c674
asher <alexetik@yandex.ru>
2025-01-13 07:12:32 +0000

    правки

--- src/main/java/obuch/SimpleConsumer1.java
+++ src/main/java/obuch/SimpleConsumer1.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.Set;
 
 @Slf4j
 public class SimpleConsumer1 {
@@ -18,9 +20,25 @@ public class SimpleConsumer1 {
         props.put("group.id", "test-group");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "earliest");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Collections.singletonList("topik13"));
+
+        consumer.poll(Duration.ofMillis(0));
+
+
+        Set<TopicPartition> assignedPartitions = consumer.assignment();
+
+
+        while (assignedPartitions.isEmpty()) {
+            consumer.poll(Duration.ofMillis(100));
+            assignedPartitions = consumer.assignment();
+        }
+
+        for (TopicPartition partition : assignedPartitions) {
+            consumer.seek(partition, 2);
+        }
 
         try {
             while (true) {
