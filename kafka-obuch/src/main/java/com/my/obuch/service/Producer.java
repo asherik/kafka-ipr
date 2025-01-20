624dbe5e7dfa2a134266d285ef9f3a488cb4b0bf
asher <alexetik@yandex.ru>
2025-01-20 12:19:33 +0000

    правки

--- kafka-obuch/src/main/java/com/my/obuch/service/Producer.java
+++ kafka-obuch/src/main/java/com/my/obuch/service/Producer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -11,11 +12,13 @@ public class Producer {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @SneakyThrows
+    @Transactional
     public void sendMessage() {
         int i = 8;
         for (int j = 0; j < i; j++) {
             kafkaTemplate.send("testik", "sadsadsa");
             Thread.sleep(2000);
+
         }
     }
 }
