2d5d20009eee715c62f1847484f3144ecad64440
asher <alexetik@yandex.ru>
2025-01-20 12:59:13 +0000

    правки

--- kafka-obuch/src/main/java/com/my/obuch/service/Consumer.java
+++ kafka-obuch/src/main/java/com/my/obuch/service/Consumer.java
@@ -2,7 +2,9 @@ package com.my.obuch.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,12 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class Consumer {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @KafkaListener(topics = "testik", groupId = "testik_group")
     @Transactional
     public void listenGroupTestik(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
         try {
 
             log.info("Получено сообщение: {}", record.value());
+
+
+            kafkaTemplate.send("другая_тема", "ответное сообщение");
 
 
             acknowledgment.acknowledge();
