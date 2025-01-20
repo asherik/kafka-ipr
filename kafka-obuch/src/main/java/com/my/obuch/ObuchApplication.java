2d600d020c4d7c281615f5a21943d9250a75feb0
asher <alexetik@yandex.ru>
2025-01-20 07:32:12 +0000

    правки

---
+++ kafka-obuch/src/main/java/com/my/obuch/ObuchApplication.java
@@ -0,0 +1,13 @@
+package com.my.obuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObuchApplication.class, args);
+	}
+
+}
---
+++ kafka-obuch/src/test/java/com/my/obuch/ObuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.obuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
